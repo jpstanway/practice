bcc9ab94819cf808ccf2039ecc2883b18fbd83ae
Jordan S <jpstanway@gmail.com>
2019-06-05 15:16:55 -0600

    set up custom array types with arguments for users and posts

--- graphql-course/graphql-basics/src/index.js
+++ graphql-course/graphql-basics/src/index.js
@@ -2,9 +2,53 @@ import { GraphQLServer } from "graphql-yoga";
 
 // 5 Scalar Types = String, Boolean, Int, Float, ID
 
+// Demo user data
+const users = [
+  {
+    id: "1",
+    name: "Andrew",
+    email: "andrew@ewxample.com",
+    age: 27
+  },
+  {
+    id: "2",
+    name: "Sara",
+    email: "sara@example.com"
+  },
+  {
+    id: "3",
+    name: "Mike",
+    email: "mike@example.com"
+  }
+];
+
+// Demo post data
+const posts = [
+  {
+    id: 100,
+    title: "How to train your dog",
+    body: "Here are 10 things to teach your dog",
+    published: true
+  },
+  {
+    id: 155,
+    title: "Why you should not eat sugar",
+    body: "5 reasons sugar is bad for you",
+    published: false
+  },
+  {
+    id: 366,
+    title: "Summertime in Calgary",
+    body: "Things to do in Calgary during the hot months",
+    published: true
+  }
+];
+
 // Type definitions (schema)
 const typeDefs = `
     type Query {
+        users(query: String): [User!]!
+        posts(query: String): [Post!]!
         me: User!
         post: Post!
     }
@@ -27,6 +71,27 @@ const typeDefs = `
 // Resolvers
 const resolvers = {
   Query: {
+    users(parent, args, ctx, info) {
+      if (!args.query) {
+        return users;
+      }
+
+      return users.filter(user => {
+        return user.name.toLowerCase().includes(args.query.toLowerCase());
+      });
+    },
+    posts(parent, args, ctx, info) {
+      if (!args.query) {
+        return posts;
+      }
+
+      return posts.filter(post => {
+        return (
+          post.title.toLowerCase().includes(args.query.toLowerCase()) ||
+          post.body.toLowerCase().includes(args.query.toLowerCase())
+        );
+      });
+    },
     me() {
       return {
         id: "123098",
