d997f59a508bea58c3d89a7bd34765f3844c5e53
Jordan S <jpstanway@gmail.com>
2019-06-09 17:02:46 -0600

    create update mutations for users, posts and comments

---
+++ graphql-course/graphql-basics/src/resolvers/Query.js
@@ -0,0 +1,44 @@
+const Query = {
+  users(parent, args, { db }, info) {
+    if (!args.query) {
+      return db.users;
+    }
+
+    return db.users.filter(user => {
+      return user.name.toLowerCase().includes(args.query.toLowerCase());
+    });
+  },
+  posts(parent, args, { db }, info) {
+    if (!args.query) {
+      return db.posts;
+    }
+
+    return db.posts.filter(post => {
+      return (
+        post.title.toLowerCase().includes(args.query.toLowerCase()) ||
+        post.body.toLowerCase().includes(args.query.toLowerCase())
+      );
+    });
+  },
+  comments(parent, args, { db }, info) {
+    return db.comments;
+  },
+  me() {
+    return {
+      id: "123098",
+      name: "Mike",
+      email: "mike@example.com",
+      age: 28
+    };
+  },
+  post() {
+    return {
+      id: "987qwerty",
+      title: "Where is the gym?",
+      body: "10 gyms near Calgary centre",
+      published: true
+    };
+  }
+};
+
+export { Query as default };
