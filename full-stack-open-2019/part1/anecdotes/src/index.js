eff429a7b2feee57ff439b7c2092c0e29c8c6eba
Jordan S <jpstanway@gmail.com>
2019-08-26 09:22:12 -0600

    complete anecdote project

--- full-stack-open-2019/part1/anecdotes/src/index.js
+++ full-stack-open-2019/part1/anecdotes/src/index.js
@@ -4,16 +4,33 @@ import "./index.css";
 
 const App = props => {
   const [selected, setSelected] = useState(0);
+  const [likesArray, setLikesArray] = useState(props.anecdotes.map(() => 0));
 
   const handleNextClick = () => {
     const randomNum = Math.floor(Math.random() * props.anecdotes.length);
     setSelected(randomNum);
   };
 
+  const handleLikeClick = () => {
+    setLikesArray(
+      likesArray.map((value, idx) => {
+        if (idx === selected) {
+          return (value += 1);
+        }
+        return value;
+      })
+    );
+  };
+
   return (
     <div className="container" style={{ padding: "20px" }}>
+      <h1>Anecdote of the day</h1>
       <div>{props.anecdotes[selected]}</div>
+      <p>has {likesArray[selected]} votes</p>
+      <button onClick={handleLikeClick}>vote</button>
       <button onClick={handleNextClick}>next anecdote</button>
+      <h1>Anecdote with the most votes</h1>
+      <div>{props.anecdotes[likesArray.indexOf(Math.max(...likesArray))]}</div>
     </div>
   );
 };
