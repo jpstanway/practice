4f6b7b502e0c00174e57a0819dfd3506b2fcc531
Jordan S <jpstanway@gmail.com>
2019-08-27 11:55:21 -0600

    implement firebase utils, include ability to store new users in firestore

--- full-stack-react-2019/crwn-clothing/src/firebase/firebase.utils.js
+++ full-stack-react-2019/crwn-clothing/src/firebase/firebase.utils.js
@@ -12,6 +12,32 @@ const config = {
   appId: "1:944128887110:web:25a89af22f1607b9"
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log("error creating user", error.message);
+    }
+  }
+
+  return userRef;
+};
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
