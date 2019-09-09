2e8167efac71eb9ad8942a18f2acb66bf415d01e
Jordan S <jpstanway@gmail.com>
2019-09-08 18:14:37 -0700

    retrieve weather report for capital city when viewing single country

--- full-stack-open-2019/part2/dataforcountries/src/components/Country.js
+++ full-stack-open-2019/part2/dataforcountries/src/components/Country.js
@@ -1,8 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Country = ({
   country: { name, capital, population, languages, flag }
 }) => {
+  const [weather, setWeather] = useState("");
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.apixu.com/v1/current.json?key=a9f4a65f23624465b6805127190909&q=${capital}`
+      )
+      .then(res => setWeather(res.data));
+  }, []);
+
+  const renderWeather = () => {
+    if (weather) {
+      return (
+        <div>
+          <h2>Weather in {capital}</h2>
+          <p>
+            <strong>temperature:</strong> {weather.current.temp_c} Celsius
+          </p>
+          <img src={weather.current.condition.icon} alt="weather icon" />
+          <p>
+            <strong>wind:</strong> {weather.current.wind_kph} kph direction{" "}
+            {weather.current.wind_dir}
+          </p>
+        </div>
+      );
+    }
+    return <div></div>;
+  };
+  console.log(weather);
   return (
     <li key={name}>
       <h1>{name}</h1>
@@ -19,6 +49,7 @@ const Country = ({
         alt="flag"
         style={{ maxWidth: "150px", marginTop: "30px" }}
       />
+      {renderWeather()}
     </li>
   );
 };
