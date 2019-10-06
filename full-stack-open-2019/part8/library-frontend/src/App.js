cb72cb234f01952f3356fba9e771d830492314bf
Jordan S <jpstanway@gmail.com>
2019-10-06 15:39:25 -0700

    create queries for all authors and all books and display results in components

--- full-stack-open-2019/part8/library-frontend/src/App.js
+++ full-stack-open-2019/part8/library-frontend/src/App.js
@@ -1,33 +1,50 @@
-import React, { useState } from 'react'
-import Authors from './components/Authors'
-import Books from './components/Books'
-import NewBook from './components/NewBook'
+import React, { useState } from "react";
+import { Query } from "react-apollo";
+import { gql } from "apollo-boost";
+
+import Authors from "./components/Authors";
+import Books from "./components/Books";
+import NewBook from "./components/NewBook";
+
+const ALL_AUTHORS = gql`
+  query {
+    allAuthors {
+      name
+      born
+      bookCount
+    }
+  }
+`;
+
+const ALL_BOOKS = gql`
+  query {
+    allBooks {
+      title
+      author
+      published
+    }
+  }
+`;
 
 const App = () => {
-  const [page, setPage] = useState('authors')
+  const [page, setPage] = useState("authors");
 
   return (
     <div>
       <div>
-        <button onClick={() => setPage('authors')}>authors</button>
-        <button onClick={() => setPage('books')}>books</button>
-        <button onClick={() => setPage('add')}>add book</button>
+        <button onClick={() => setPage("authors")}>authors</button>
+        <button onClick={() => setPage("books")}>books</button>
+        <button onClick={() => setPage("add")}>add book</button>
       </div>
-
-      <Authors
-        show={page === 'authors'}
-      />
-
-      <Books
-        show={page === 'books'}
-      />
-
-      <NewBook
-        show={page === 'add'}
-      />
-
+      <Query query={ALL_AUTHORS}>
+        {result => <Authors show={page === "authors"} result={result} />}
+      </Query>
+      <Query query={ALL_BOOKS}>
+        {result => <Books show={page === "books"} result={result} />}
+      </Query>
+      <NewBook show={page === "add"} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- full-stack-open-2019/part8/library-frontend/src/components/Authors.js
+++ full-stack-open-2019/part8/library-frontend/src/components/Authors.js
@@ -1,10 +1,15 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
-const Authors = (props) => {
-  if (!props.show) {
-    return null
+const Authors = ({ show, result }) => {
+  if (!show) {
+    return null;
   }
-  const authors = []
+
+  if (result.loading) {
+    return <div>loading...</div>;
+  }
+
+  const authors = result.data.allAuthors;
 
   return (
     <div>
@@ -13,25 +18,20 @@ const Authors = (props) => {
         <tbody>
           <tr>
             <th></th>
-            <th>
-              born
-            </th>
-            <th>
-              books
-            </th>
+            <th>born</th>
+            <th>books</th>
           </tr>
-          {authors.map(a =>
+          {authors.map(a => (
             <tr key={a.name}>
               <td>{a.name}</td>
               <td>{a.born}</td>
               <td>{a.bookCount}</td>
             </tr>
-          )}
+          ))}
         </tbody>
       </table>
-
     </div>
-  )
-}
+  );
+};
 
-export default Authors
+export default Authors;
