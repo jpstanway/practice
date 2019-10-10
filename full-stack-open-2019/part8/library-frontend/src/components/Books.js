f76de355cd4900012a196b5302ef68c193fac1d6
Jordan S <jpstanway@gmail.com>
2019-10-09 19:43:50 -0700

    create optional filter for displayed books

--- full-stack-open-2019/part8/library-frontend/src/components/Books.js
+++ full-stack-open-2019/part8/library-frontend/src/components/Books.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Books = ({ show, result }) => {
+  const [filter, setFilter] = useState("all genres");
+
   if (!show) {
     return null;
   }
@@ -10,11 +12,13 @@ const Books = ({ show, result }) => {
   }
 
   const books = result.data.allBooks;
-
+  console.log(books);
   return (
     <div>
       <h2>books</h2>
-
+      <p>
+        in genre: <strong>{filter}</strong>
+      </p>
       <table>
         <tbody>
           <tr>
@@ -22,15 +26,27 @@ const Books = ({ show, result }) => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.map(a => (
-            <tr key={a.title}>
-              <td>{a.title}</td>
-              <td>{a.author.name}</td>
-              <td>{a.published}</td>
-            </tr>
-          ))}
+          {books
+            .filter(b =>
+              filter !== "all genres" ? b.genres.includes(filter) : b
+            )
+            .map(a => (
+              <tr key={a.title}>
+                <td>{a.title}</td>
+                <td>{a.author.name}</td>
+                <td>{a.published}</td>
+              </tr>
+            ))}
         </tbody>
       </table>
+      <div>
+        <button onClick={() => setFilter("crime")}>crime</button>
+        <button onClick={() => setFilter("conspiracy")}>conspiracy</button>
+        <button onClick={() => setFilter("scifi")}>scifi</button>
+        <button onClick={() => setFilter("science")}>science</button>
+        <button onClick={() => setFilter("horror")}>horror</button>
+        <button onClick={() => setFilter("all genres")}>all genres</button>
+      </div>
     </div>
   );
 };
