1369203ff7144fb3b50564b01f82a306d5f83d58
Jordan S <jpstanway@gmail.com>
2019-11-19 20:29:23 -0800

    convert some components into styled components

--- full-stack-react-2019/crwn-clothing/src/components/header/header.component.jsx
+++ full-stack-react-2019/crwn-clothing/src/components/header/header.component.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 
@@ -10,33 +9,39 @@ import { selectCartHidden } from "../../redux/cart/cart.selectors";
 import { selectCurrentUser } from "../../redux/user/user.selectors";
 
 import { ReactComponent as Logo } from "../../assets/crown.svg";
-import "./header.styles.scss";
+
+import {
+  HeaderContainer,
+  LogoContainer,
+  OptionsContainer,
+  OptionLink
+} from "./header.styles";
 
 const Header = ({ currentUser, hidden }) => (
-  <div className="header">
-    <Link to="/" className="logo-container">
+  <HeaderContainer>
+    <LogoContainer to="/" className="logo-container">
       <Logo className="logo" />
-    </Link>
-    <div className="options">
-      <Link to="shop" className="option">
+    </LogoContainer>
+    <OptionsContainer>
+      <OptionLink to="shop" className="option">
         SHOP
-      </Link>
-      <Link to="shop" className="option">
+      </OptionLink>
+      <OptionLink to="shop" className="option">
         CONTACT
-      </Link>
+      </OptionLink>
       {currentUser ? (
-        <div className="option" onClick={() => auth.signOut()}>
+        <OptionLink as="div" onClick={() => auth.signOut()}>
           SIGN OUT
-        </div>
+        </OptionLink>
       ) : (
-        <Link className="option" to="/signin">
+        <OptionLink className="option" to="/signin">
           SIGN IN
-        </Link>
+        </OptionLink>
       )}
       <CartIcon />
-    </div>
+    </OptionsContainer>
     {hidden ? null : <CartDropdown />}
-  </div>
+  </HeaderContainer>
 );
 
 const mapStateToProps = createStructuredSelector({
