f2c84ea7392a42165bef04998cb4a29f8065636a
Jordan S <jpstanway@gmail.com>
2020-04-08 19:59:13 -0700

    add union method to set class to combine another set

--- challenges/set_class.js
+++ challenges/set_class.js
@@ -1,39 +1,42 @@
-class Set {
-  constructor() {
-    // collection will hold our set
-    this.collection = [];
-  }
+function Set() {
+  // the var collection will hold the set
+  var collection = [];
   // this method will check for the presence of an element and return true or false
-  has(element) {
-    return this.collection.indexOf(element) !== -1;
-  }
-
-  values() {
-    return this.collection;
-  }
-
-  add(element) {
-    if (this.collection.indexOf(element) > -1) {
-      return false;
-    }
-
-    this.collection.push(element);
-    return true;
-  }
-
-  remove(element) {
-    let index = this.collection.indexOf(element);
-
-    // check that element exists
-    if (index > -1) {
-      this.collection.splice(index, 1);
+  this.has = function (element) {
+    return collection.indexOf(element) !== -1;
+  };
+  // this method will return all the values in the set
+  this.values = function () {
+    return collection;
+  };
+  // this method will add an element to the set
+  this.add = function (element) {
+    if (!this.has(element)) {
+      collection.push(element);
       return true;
     }
-
     return false;
-  }
+  };
+  // this method will remove an element from a set
+  this.remove = function (element) {
+    if (this.has(element)) {
+      var index = collection.indexOf(element);
+      collection.splice(index, 1);
+      return true;
+    }
+    return false;
+  };
+  // this method will return the size of the set
+  this.size = function () {
+    return collection.length;
+  };
+  // this method will combine a second set into a new set
+  this.union = function (set2) {
+    const setUnion = new Set();
+    const addElementsToSet = (element) => setUnion.add(element);
 
-  size() {
-    return this.collection.length;
-  }
+    this.values().forEach(addElementsToSet);
+    set2.values().forEach(addElementsToSet);
+    return setUnion;
+  };
 }
