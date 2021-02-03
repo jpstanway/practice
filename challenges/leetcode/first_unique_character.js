029c1769bdd69c7c92886c999b23e2c1aa4eac2f
Jordan S <jpstanway@gmail.com>
2021-02-03 07:56:00 -0800

    complete first unique character in a string challenge

---
+++ challenges/leetcode/first_unique_character.js
@@ -0,0 +1,9 @@
+var firstUniqChar = function(s) {
+  for(let i = 0; i < s.length; i++) {
+      if (s.indexOf(s[i]) === i && s.lastIndexOf(s[i]) === i) {
+          return i;
+      }
+  }
+  
+  return -1;
+};
