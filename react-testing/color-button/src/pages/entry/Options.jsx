82c58b08619f32bf6385506728a64f40cd135310
Jordan S <jpstanway@gmail.com>
2021-10-09 18:14:18 -0700

    write tests for alert component

--- react-testing/color-button/src/pages/entry/Options.jsx
+++ react-testing/color-button/src/pages/entry/Options.jsx
@@ -2,21 +2,25 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import { Row } from "react-bootstrap";
 import ScoopOption from "./ScoopOption";
+import ToppingOption from "./ToppingOption";
+import AlertBanner from "../common/AlertBanner";
 
 export default function Options({ optionType }) {
   const [items, setItems] = useState([]);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     axios
       .get(`http://localhost:3030/${optionType}`)
       .then((response) => setItems(response.data))
-      .catch((error) => {
-        // TODO: handle errors
-      });
+      .catch((error) => setError(true));
   }, [optionType]);
 
-  // TODO: replace null with ToppingOption
-  const ItemComponent = optionType === "scoops" ? ScoopOption : null;
+  if (error) {
+    return <AlertBanner />;
+  }
+
+  const ItemComponent = optionType === "scoops" ? ScoopOption : ToppingOption;
   const optionItems = items.map((item) => (
     <ItemComponent
       key={item.name}
