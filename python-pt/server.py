56898ccdca3a80c241b9050d62101edc2d0cb111
Jordan S <jpstanway@gmail.com>
2022-04-14 16:52:28 -0700

    create nmap scanner

--- python-pt/server.py
+++ python-pt/server.py
@@ -5,7 +5,7 @@ serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host = socket.gethostbyname()
 port = 444
 
-serversocket.bind(host, port)
+serversocket.bind((host, port))
 
 serversocket.listen(1)
 
@@ -15,6 +15,6 @@ while True:
     print('Received connection from ' % str(address))
 
     message = 'Hello! Thank you for connecting to the server' + '\r\n'
-    clientsocket.send(message)
+    clientsocket.send(message.encode('ascii'))
 
     clientsocket.close()
